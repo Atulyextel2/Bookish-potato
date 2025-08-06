9d45baa1beca6411fb54192a2b370432a1e0a90c
Atul Singh <atulsingh@Atuls-MacBook-Pro.local>
2025-08-07 00:07:29 +0530

    created the card prefab

--- Assets/_0_Main/Scripts/Implementations/CardView.cs
+++ Assets/_0_Main/Scripts/Implementations/CardView.cs
@@ -1,9 +1,32 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CardView : MonoBehaviour
 {
-    public void Initialize(Card model, Sprite face, Sprite back) 
-    { 
-        
+    private Card card;
+    [SerializeField] private Image cardFrontImage, cardBackImage;
+
+    public void Initialize(Card card, Sprite face, Sprite back)
+    {
+        if (card != null)
+        {
+            this.card = card;
+        }
+
+        if (cardFrontImage != null)
+        {
+            if (face != null)
+            {
+                cardFrontImage.sprite = face;
+            }
+        }
+
+        if (cardBackImage != null)
+        {
+            if (back != null)
+            {
+                cardBackImage.sprite = back;
+            }
+        }
     }
 }
